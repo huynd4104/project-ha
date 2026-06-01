fb74a116480705a49adef1a71752a5c2baefd3af
huynd4104 <huy412004@gmail.com>
2026-06-02 04:19:01 +0700

    feat: replace placeholder icon with mascot asset in conversation panel

--- mobile-flutter/lib/features/ai_conversation/presentation/child/widgets/ai_conversation_mascot_panel.dart
+++ mobile-flutter/lib/features/ai_conversation/presentation/child/widgets/ai_conversation_mascot_panel.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../core/constants/mascot_assets.dart';
 import '../../../../../core/theme/app_colors.dart';
 import '../../../../../core/theme/app_text_styles.dart';
 import '../../../../../core/widgets/app_card.dart';
+import '../../../../../core/widgets/mascot_image.dart';
 
 class AiConversationMascotPanel extends StatelessWidget {
   const AiConversationMascotPanel({
@@ -20,13 +22,13 @@ class AiConversationMascotPanel extends StatelessWidget {
     borderColor: AppColors.yellow.withValues(alpha: .35),
     child: Row(
       children: [
-        const CircleAvatar(
-          radius: 34,
-          backgroundColor: AppColors.yellow,
-          child: Icon(
-            Icons.auto_awesome_rounded,
-            color: AppColors.text,
-            size: 34,
+        const SizedBox(
+          width: 68,
+          height: 68,
+          child: MascotImage(
+            assetPath: MascotAssets.mascotMain,
+            fit: BoxFit.contain,
+            animate: false,
           ),
         ),
         const SizedBox(width: 14),
